e5579593d3ddbdd6498e81ff1dd339844977017d
Edson Silva <silva.email@yahoo.com>
2024-02-06 20:23:48 -0500

    Add sillyname and superheroes modules

---
+++ index.js
@@ -0,0 +1,14 @@
+// var generateName = require("sillyname")
+
+
+// import generateName from "sillyname"
+// var sillyName = generateName();
+
+import superheroes from "superheroes"
+// superheroes.all
+
+
+const name = superheroes.random()
+
+// console.log(`My name is ${sillyName}`)
+console.log(`I am ${name}!!!!`)
